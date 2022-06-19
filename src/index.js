3f5c5f8887632aa58f8361bce61e1213578844f7
Colleen Murray <colleenkhm@gmail.com>
2022-06-19 14:52:47 -0500

    about section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
