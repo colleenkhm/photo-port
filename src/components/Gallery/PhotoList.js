feead04cf6ecce2d79490a0c294eadbf9c98281a
Colleen Murray <colleenkhm@gmail.com>
2022-06-19 19:23:27 -0500

    import and instantiate useState and useEffect

---
+++ src/components/Gallery/PhotoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import photo from '../../assets/small/commercial/0.jpg';
+
+function PhotoList() {
+    return (
+        <div>
+            <img
+            src={photo}
+            alt="commercial Example"
+            />
+        </div>
+    )
+}
+
+export default PhotoList;
